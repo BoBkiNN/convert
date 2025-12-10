a775368c681222fed7b99c7f615b70be6733a915
p2r3 <p2r3@p2r3.com>
2025-12-10 11:12:22 +0000

    add support for text to image rendering

--- handlers/canvasToBlob.js
+++ handlers/canvasToBlob.js
@@ -34,6 +34,15 @@ const supportedFormats = [
     from: true,
     to: false,
     internal: "gif"
+  },
+  {
+    name: "Plain Text",
+    format: "text",
+    extension: "txt",
+    mime: "text/plain",
+    from: true,
+    to: false,
+    internal: "text"
   }
 ];
 
@@ -46,21 +55,44 @@ async function init () {
 
 async function doConvert (inputFile, inputFormat, outputFormat) {
 
-  const blob = new Blob([inputFile.bytes], { type: inputFormat.mime });
-  const url = URL.createObjectURL(blob);
+  if (inputFormat.mime === "text/plain") {
 
-  let image = document.createElement("img");
-  await new Promise((resolve, reject) => {
-    image.addEventListener("load", resolve);
-    image.src = url;
-  });
+    const font = "48px sans-serif";
+    const string = new TextDecoder().decode(inputFile.bytes);
 
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  ctx.drawImage(image, 0, 0);
+    ctx.font = font;
+    canvas.width = ctx.measureText(string).width;
+    canvas.height = parseInt(font);
+
+    if (outputFormat.mime === "image/jpeg") {
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    ctx.fillStyle = "black";
+    ctx.font = font;
+    ctx.fillText(string, 0, parseInt(font));
+
+  } else {
+
+    const blob = new Blob([inputFile.bytes], { type: inputFormat.mime });
+    const url = URL.createObjectURL(blob);
+
+    let image = document.createElement("img");
+    await new Promise((resolve, reject) => {
+      image.addEventListener("load", resolve);
+      image.addEventListener("error", reject);
+      image.src = url;
+    });
+
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+    ctx.drawImage(image, 0, 0);
+
+  }
 
   return await new Promise((resolve, reject) => {
     canvas.toBlob((blob) => {
+      if (!blob) reject("Canvas output failed");
       blob.arrayBuffer().then(buf => resolve(new Uint8Array(buf)));
     }, outputFormat.mime);
   });
